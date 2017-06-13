a5b1cf14f7d12042d26d88d5efdaff1fe432c93a
Julien Ducro <julien.ducro@deskpro.com>
2017-06-13 12:17:10 +0100

    Add max dimensions to modals

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -9,13 +9,58 @@ class Modal extends React.Component {
     style:    PropTypes.object
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      width:  0,
+      height: 0
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', () => {
+      if (!this.ticking) {
+        window.requestAnimationFrame(() => {
+          this.updateWindowDimensions();
+          this.ticking = false;
+        });
+      }
+      this.ticking = true;
+    });
+  }
+
+  componentWillUnmount() {
+    window.addEventListener('resize', () => {
+      if (!this.ticking) {
+        window.requestAnimationFrame(() => {
+          this.updateWindowDimensions();
+          this.ticking = false;
+        });
+      }
+      this.ticking = true;
+    });
+  }
+
+  updateWindowDimensions() {
+    this.setState({
+      width:  window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
   render() {
     const { title, children, buttons, style } = this.props;
+    const { width, height } = this.state;
+    const modalStyle = Object.assign({}, style);
+    modalStyle.maxHeight = height * 0.7;
+    modalStyle.maxWidth  = width * 0.7;
     return (
       <div className="dp-modal__overlay">
         <div
           className="dp-modal"
-          style={style}
+          style={modalStyle}
         >
           <div className="dp-modal__title">
             {title}
