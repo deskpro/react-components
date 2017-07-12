f0cc098486144d386ace90cf26fd4c4b0bba8bb9
headzoo <sean@headzoo.io>
2017-07-12 08:50:11 -0400

    Adds SearchSubmit component

--- src/Components/Forms/index.js
+++ src/Components/Forms/index.js
@@ -5,6 +5,7 @@ import Tag from './Tag';
 import TagInput from './TagInput';
 import Select from './Select';
 import SearchInline from './SearchInline';
+import SearchSubmit from './SearchSubmit';
 import HiddenFields from './HiddenFields';
 
-export { Checkbox, Input, Label, Tag, TagInput, Select, SearchInline, HiddenFields };
+export { Checkbox, Input, Label, Tag, TagInput, Select, SearchInline, SearchSubmit, HiddenFields };
