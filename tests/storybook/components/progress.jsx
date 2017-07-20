0415d80e01e713bd797cd2c0eb7edf2897810fc9
headzoo <sean@headzoo.io>
2017-07-19 23:32:02 -0400

    Improves Progress story

--- tests/storybook/components/progress.jsx
+++ tests/storybook/components/progress.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, number } from '@storybook/addon-knobs';
 import Progress from 'Components/Progress';
 import ProgressBar from 'Components/ProgressBar';
 
-storiesOf('Progress', module)
-  .addDecorator(withKnobs)
-  .add('Progress', () => {
-    const percent = number('Percent', 75);
+class Story extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      percent: 10
+    };
+    this.interval = null;
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      let percent = this.state.percent;
+      percent += 1;
+      if (percent > 100) {
+        percent = 0;
+      }
+      this.setState({ percent });
+    }, 100);
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+
+  render() {
+    const { percent } = this.state;
     const sizes = ['small', 'medium', 'large'];
 
     return (
-      <div>
+      <div style={{ width: '25%', margin: '0 auto' }}>
         {sizes.map(size => (
           <div>
             <Progress size={size} type="primary" style={{ margin: 10, border: '1px solid #ccc' }}>
@@ -29,5 +53,8 @@ storiesOf('Progress', module)
       </div>
     );
   }
-)
-;
+}
+
+storiesOf('Progress', module)
+  .add('Progress', () => <Story />
+);
