822a69a3d7ce34d036590da6e0e9ce477b451c01
headzoo <sean@headzoo.io>
2017-09-08 16:08:25 -0400

    Adds Container component

---
+++ src/Components/Container.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+/**
+ * Renders a container.
+ */
+const Container = ({ className, children, ...props }) => (
+  <div className={classNames('up-container', classNames)} {...props}>
+    {children}
+  </div>
+);
+
+Container.propTypes = {
+  /**
+   * CSS classes to apply to the element.
+   */
+  className: PropTypes.string,
+  /**
+   * Children to render.
+   */
+  children:  PropTypes.node
+};
+
+Container.defaultProps = {
+  className: '',
+  children:  []
+};
+
+export default Container;
