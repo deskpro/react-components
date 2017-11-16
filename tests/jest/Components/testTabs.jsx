00ed8b5e88b99a379964c51de5bdbfc8a1fb7f35
headzoo <sean@headzoo.io>
2017-11-16 15:32:48 -0500

    Adds tests for tab components

---
+++ tests/jest/Components/testTabs.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Tabs, TabLink } from 'Components/Tabs/index';
+
+describe('>>> Tabs --- Snapshot', () => {
+  it('+++capturing Snapshot of TabLink', () => {
+    const renderedValue = renderer.create(
+      <TabLink name="one">
+        Tab One
+      </TabLink>
+    ).toJSON();
+    expect(renderedValue).toMatchSnapshot();
+  });
+  it('+++capturing Snapshot of Tabs', () => {
+    const renderedValue = renderer.create(
+      <Tabs active="one">
+        <TabLink name="one">
+          Tab One
+        </TabLink>
+        <TabLink name="two">
+          Tab Two
+        </TabLink>
+      </Tabs>
+    ).toJSON();
+    expect(renderedValue).toMatchSnapshot();
+  });
+});
