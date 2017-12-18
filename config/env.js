aafa5ddb7ec7f589f2745d8718dcb3e5fa1acb10
Julien Ducro <julien.ducro@deskpro.com>
2017-12-18 13:34:03 +0000

    Fix number of days in the month + npm update

--- config/env.js
+++ config/env.js
@@ -8,11 +8,9 @@ const dotenv = require('dotenv');
 // Make sure that including paths.js after env.js will read .env variables.
 delete require.cache[require.resolve('./paths')];
 
-const NODE_ENV = process.env.NODE_ENV;
+const { NODE_ENV } = process.env;
 if (!NODE_ENV) {
-  throw new Error(
-    'The NODE_ENV environment variable is required but was not specified.'
-  );
+  throw new Error('The NODE_ENV environment variable is required but was not specified.');
 }
 
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
@@ -66,16 +64,16 @@ function getClientEnvironment(publicUrl) {
         env[key] = process.env[key];
         return env;
       },
-    {
+      {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
-      NODE_ENV:   process.env.NODE_ENV || 'development',
+        NODE_ENV:   process.env.NODE_ENV || 'development',
         // Useful for resolving the correct path to static assets in `public`.
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
-      PUBLIC_URL: publicUrl,
-    }
+        PUBLIC_URL: publicUrl,
+      }
     );
   // Stringify all values so we can feed into Webpack DefinePlugin
   const stringified = {
